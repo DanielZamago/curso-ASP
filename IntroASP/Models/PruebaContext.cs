3636f940f45b365db2c17bacb64075b52a75421e
cris_ <cris_@CHIMUELA>
2023-02-28 19:13:30 -0700

    Agregar archivos de proyecto.

---
+++ IntroASP/Models/PruebaContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroASP.Models;
+
+public partial class PruebaContext : DbContext
+{
+    public PruebaContext()
+    {
+    }
+
+    public PruebaContext(DbContextOptions<PruebaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Beer> Beers { get; set; }
+
+    public virtual DbSet<Brand> Brands { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=CHIMUELA; Database=prueba; Trusted_Connection=True; TrustServerCertificate=True ");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Beer>(entity =>
+        {
+            entity.HasKey(e => e.BeerId).HasName("PK__Beer__293C94BFE3274006");
+
+            entity.ToTable("Beer");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("name");
+
+            entity.HasOne(d => d.Brand).WithMany(p => p.Beers)
+                .HasForeignKey(d => d.BrandId)
+                .HasConstraintName("BrandId");
+        });
+
+        modelBuilder.Entity<Brand>(entity =>
+        {
+            entity.HasKey(e => e.BrandId).HasName("PK__Brand__DAD4F05E755F79B3");
+
+            entity.ToTable("Brand");
+
+            entity.Property(e => e.BrandId).ValueGeneratedNever();
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
